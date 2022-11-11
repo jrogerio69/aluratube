9eb91ba11d8fc1be737afc3e9dd88664b568d865
Jose Rogerio <jrogerio69@gmail.com>
2022-11-11 19:17:50 -0300

    Commit da segunda aula

--- src/components/Menu/components/Search.js
+++ src/components/Menu/components/Search.js
@@ -39,10 +39,15 @@ const StyledSearch = styled.div`
 // Search
 // Informação sempre desce
 
-export default function Search() {
+export default function Search({ valorDoFiltro, setValorDoFiltro }) {
+    // const [valorDaBusca, setValorDaBusca] = React.useState("Teste");
+    // console.log("Search", valorDaBusca)
+    const valorDaBusca = valorDoFiltro;
+    const setValorDaBusca = setValorDoFiltro;
+
     return (
         <StyledSearch>
-            <input type="text" />
+            <input type="text" onChange={(e) => setValorDaBusca(e.target.value)} value={valorDaBusca} />
             <button>
                 🔎
             </button>
